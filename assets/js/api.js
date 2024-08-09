fc8012d95de3eaef48a265f444901ff0c26585f7
Danrley <danrleyifba@gmail.com>
2024-08-08 23:21:18 -0300

    Site

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/DanrleyS/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/DanrleyS/site/main/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
